3e09adc060e497ac38cd8edf266c32ed30f1f1f5
lcb931023 <lcb931023@gmail.com>
2017-08-09 02:58:15 -0700

    visualize sim on a grid of numbers

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const FluidDynamicsSolver = require('./lib/fds')
 
+const B_SHOW_FLUID = false
+
 const N_ = 30 // sim complexity...?
 const solver = new FluidDynamicsSolver(N_)
 solver.initFDS()
 
-const grid_width  = N_;
-const grid_height = N_;
+const container = document.createElement('div')
+container.classList.add('container')
+document.body.appendChild(container)
 
 const canvas = document.createElement('canvas')
 const RESOLUTION = 512
@@ -15,23 +18,46 @@ canvas.setAttribute('width', RESOLUTION)
 canvas.setAttribute('height', RESOLUTION)
 // canvas.style.margin = '10px'
 canvas.style.background = 'yellow'
-document.body.appendChild(canvas)
+if (!B_SHOW_FLUID) {
+  canvas.style.opacity = 0
+}
+canvas.style.border = '1px solid white'
+container.appendChild(canvas)
 const canvasContext = canvas.getContext('2d')
 
 // NOTE copy the canvas element and it's context
 // used for double buffering
 const canvasCopy = document.createElement('canvas')
-canvasCopy.width = grid_width;
-canvasCopy.height = grid_height;
+canvasCopy.width = N_;
+canvasCopy.height = N_;
 const canvasCopyContext = canvasCopy.getContext('2d');
-const canvasCopyImageData = canvasCopyContext.createImageData(grid_width, grid_height);
+const canvasCopyImageData = canvasCopyContext.createImageData(N_, N_);
 
+// our own renderer
+const renderGrid = document.createElement('div')
+renderGrid.classList.add('renderGrid')
+container.appendChild(renderGrid)
+const arrCells = []
+// j: y, i: x
+for(let j = 0; j < N_; j++)
+{
+  const row = document.createElement('div')
+  for(let i = 0; i < N_; i++)
+  {
+    const cell = document.createElement('div')
+    cell.innerText = '1'
+    cell.classList.add('cell')
+    cell.style.display = 'inline-block'
+    row.appendChild(cell)
+    arrCells.push(cell)
+  }
+  renderGrid.appendChild(row)
+}
 
 // interaction
 const E_POINTER_MOVE = ('ontouchstart' in window) ? 'touchmove' : 'mousemove'
 const INV_CANVAS_W = 1 / RESOLUTION
 const INV_CANVAS_H = 1 / RESOLUTION
-console.log(canvas)
 
 let prevX = 0
 let prevY = 0
@@ -76,40 +102,53 @@ function draw()
 {
   // update fluid solver
   solver.update();
-  // draw density to canvas element
+  // visualize
   drawDensity();
   
   // loop
   window.requestAnimationFrame(draw)
 }
-// draw fluid in the canvas element
+
 function drawDensity()
 {
   var img_data_i;
   var dens_index;
-  
+
+  // NOTE Start here
+  // the simulation gers rendered onto ImageData
+  // and then that imageData gets blit onto display canvas
+
   // i: column, j: row
-  for(var j = 1; j < (N_ + 1); j++)
+  // 30 of each
+  for(var j = 0; j < N_; j++)
   {
-    for(var i = 1; i < (N_ + 1); i++)
+    for(var i = 0; i < N_; i++)
     {
       // in case you need to know what that is:
       // https://developer.mozilla.org/En/HTML/Canvas/Pixel_manipulation_with_canvas
-      img_data_i = ((j - 1) * N_ * 4) + ((i - 1) * 4);
-      dens_index = solver.REG_IX(i, j);
+      img_data_i = (j * N_ * 4) + (i * 4);
+      dens_index = solver.REG_IX(i+1, j+1);
       const dens = solver.getDens(dens_index)
-      // write color to canvas
-      canvasCopyImageData.data[img_data_i]     = dens;
-      canvasCopyImageData.data[img_data_i + 1] = dens;
-      canvasCopyImageData.data[img_data_i + 2] = dens;
-      canvasCopyImageData.data[img_data_i + 3] = 255; 
+      if (B_SHOW_FLUID) {
+        // write color to canvas
+        canvasCopyImageData.data[img_data_i]     = dens;
+        canvasCopyImageData.data[img_data_i + 1] = dens;
+        canvasCopyImageData.data[img_data_i + 2] = dens;
+        canvasCopyImageData.data[img_data_i + 3] = 255; 
+      }
+
+      let cappedDens = (dens * 0.05).toFixed(0)
+      if (cappedDens > 9) cappedDens = 9
+      arrCells[ (j * N_ + i) ].innerText = cappedDens
     }
   }
   
-  // write new data back to the copy
-  canvasCopyContext.putImageData(canvasCopyImageData, 0, 0);
-  // draw image on screen using the canvas copy
-  canvasContext.drawImage(canvasCopy, 0, 0, RESOLUTION, RESOLUTION);
+  if (B_SHOW_FLUID) {
+    // write new data back to the copy
+    canvasCopyContext.putImageData(canvasCopyImageData, 0, 0);
+    // draw image on screen using the canvas copy
+    canvasContext.drawImage(canvasCopy, 0, 0, RESOLUTION, RESOLUTION);
+  }
 }
 
 draw()
